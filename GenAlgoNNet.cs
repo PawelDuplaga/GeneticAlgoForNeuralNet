35d5ea93a9f54c4306c718937ac3bf3c0abea5a3
PawelDuplaga <pawelduplagait@gmail.com>
2023-03-13 17:31:15 +0100

    + roulette selector

---
+++ GenAlgoNNet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatrixAlgebraForDoubles;
+using NeuralNetworkManager;
+
+namespace GeneticAlgoForNeuralNet
+{
+    internal class GenAlgoNNet
+    {
+        public int populationSize = 100;
+
+
+
+
+
+
+
+
+
+        public NeuralNetwork? RouletteSelection(NeuralNetwork[] generation)
+        {
+
+            double totafitness = generation.Sum(x => x.fintess);
+            double roulettePosition = new Random().NextDouble();
+            double cumulativeFitness = 0;
+
+            foreach(var net in generation)
+            {
+                cumulativeFitness += net.fintess / totafitness;
+                if(cumulativeFitness >= roulettePosition) 
+                {
+                    return net;
+                }
+            }
+
+            return null;
+        }
+
+        
+
+
+    }
+}
